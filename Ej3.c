c744d6342568642b9cf2990eab86592980302745
ShannonNCM <shannyotaku@gmail.com>
2020-03-27 21:46:39 -0600

    Segunda version

--- Ej3.c
+++ Ej3.c
@@ -11,9 +11,8 @@ Salidas: Enteros, vectores
   
 //Librerias
 #include <stdio.h>
-//Se definen las funciones a utilizar
-//Funcion magnitud
-
+#include <stdlib.h>
+#include <math.h>
 //Inicio del programa
 void main()
 {
@@ -24,7 +23,7 @@ void main()
 	//Vector U
 	puts("Coordenadas vector U");
 	for(i=0;i<3;i++){
-		printf("Ingresar valor entero: \n");
+		printf("Ingresar valor entero: ");
 		scanf("%d",&n);
 		U[i]=n;
 	}
@@ -36,7 +35,7 @@ void main()
 	//Vector V
 	puts("\n\nCoordenadas vector V");
 	for(i=0;i<3;i++){
-		printf("Ingresar valor entero: \n");
+		printf("Ingresar valor entero: ");
 		scanf("%d",&n);
 		V[i]=n;
 	}
@@ -45,4 +44,37 @@ void main()
 	for(i=0;i<3;i++){
 		printf("%d ",V[i]);
 	}
+	//Inicio de operaciones de vectores U y V
+	puts("\n\nOperaciones de Vectores");
+	//Calculo de magnitud de los vectores
+	float MAGU,MAGV;
+	//Magnitud de U
+	MAGU=sqrt((U[0]*U[0])+(U[1]*U[1])+(U[2]*U[2]));
+	printf("\nMagnitud de U: %f",MAGU);
+	//Magnitud de V
+	MAGV=sqrt((V[0]*V[0])+(V[1]*V[1])+(V[2]*V[2]));
+	printf("\nMagnitud de V: %f",MAGV);
+	//Calculo de suma de vectores
+	int S[3];
+	for(i=0;i<3;i++){
+		S[i]=U[i]+V[i];
+	}
+	//Vector suma
+	puts("\nVector suma");
+	for(i=0;i<3;i++){
+	printf("%d ",S[i]);
+	}
+	//Calculo del producto escalar
+	int PES;
+	PES=(U[0]*V[0])+(U[1]*V[1])+(U[2]*V[2]);
+	printf("\nProducto escalar: %d",PES);
+	//Calculo del producto vectorial
+	int a,b,c;
+	int PV[]={a,b,c};
+	a=(U[1]*V[2])-(U[2]*V[1]);
+	b=(U[2]*V[0])-(U[0]*V[2]);
+	c=(U[0]*V[1])-(U[1]*V[0]);
+	puts("\nProducto vectorial");
+	printf("%d %d %d ",a,b,c);
+	puts("\nFin");
 }
